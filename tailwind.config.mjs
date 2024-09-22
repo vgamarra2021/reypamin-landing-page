741b23e7e3d77d0d3ab8e3cc4f821f099628cd59
vgamarra2021 <vgamarrav@uni.pe>
2024-09-22 00:04:53 -0500

    init Contact Page

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors:{
+				'primary-red': '#E51E37',
+				'primary-blue': '#2C2D43',
+			},
+		},
+	},
+	plugins: [],
+}
