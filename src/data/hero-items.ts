57e7fc5c6430844be1c8c8dcf9d49015247251d7
vgamarra2021 <vgamarrav@uni.pe>
2024-09-30 02:28:11 -0500

    details

--- src/data/hero-items.ts
+++ src/data/hero-items.ts
@@ -5,15 +5,21 @@ export const heroItems = [
     subtitle:
       "Impulsamos la eficiencia y la mejora continua con soluciones tecnológicas avanzadas.",
     time: 500,
+    btnText: "EXPLORAR SOLUCIONES",
+    href: "/soluciones",
   },
   {
     title: "¡Innovación en cada paso!",
     subtitle: "Transforma tu industria con nuestras soluciones.",
     time: 8000,
+    btnText: "EXPLORAR SOLUCIONES",
+    href: "/soluciones",
   },
   {
     title: "¡Diseños a la medida!",
     subtitle: "Fabricamos piezas metálicas adaptadas a tus necesidades.",
     time: 15000,
+    btnText: "EXPLORAR PRODUCTOS",
+    href: "/productos",
   },
 ];
