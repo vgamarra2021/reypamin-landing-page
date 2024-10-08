d0db26b5c9d568db42e8663490e69952ef7e274b
vgamarra2021 <vgamarrav@uni.pe>
2024-10-08 18:53:37 -0500

    Update animations

--- src/functions/animation.ts
+++ src/functions/animation.ts
@@ -15,3 +15,53 @@ export function addAnimation(selector: string, className?: string) {
     elements.forEach((el) => observer.observe(el));
   });
 }
+
+export function addFadeInLettersAnimation(selector: string) {
+  const $texts = document.querySelectorAll(selector);
+  formatTitles($texts);
+
+  document.addEventListener("DOMContentLoaded", function () {
+    $texts.forEach((text) => {
+      const $letters = text.querySelectorAll("span");
+      let previousBottom = 0;
+      let auxIndex = 0;
+      $letters.forEach((span, index) => {
+        const rect = span.getBoundingClientRect();
+        if (rect.top > previousBottom) auxIndex = 0;
+        previousBottom = rect.bottom;
+        span.style.animationDelay = `${auxIndex * 20}ms`;
+        auxIndex++;
+      });
+    });
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate__animated", "animate__fadeInUp");
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+
+    $texts.forEach((text) => {
+      text
+        .querySelectorAll("span")
+        .forEach((letter) => observer.observe(letter));
+    });
+  });
+}
+
+export function formatTitle(element: HTMLElement) {
+  element.innerHTML = element.textContent!.replace(
+    /\S/g,
+    '<span class="letter">$&</span>'
+  );
+}
+
+export function formatTitles(element: HTMLElement[]) {
+  element.forEach((title: HTMLElement) => {
+    title.innerHTML = title.textContent!.replace(
+      /\S/g,
+      '<span class="letter">$&</span>'
+    );
+  });
+}
