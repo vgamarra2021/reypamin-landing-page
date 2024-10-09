4e934283c8e1446d8afa971951d982b9dc691463
vgamarra2021 <vgamarrav@uni.pe>
2024-10-09 00:37:20 -0500

    Add responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
   output: "server",
   adapter: vercel()
 });
